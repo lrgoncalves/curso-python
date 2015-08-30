1c11c3a69092d8a0c947db1c7fa59ee46f159c29
Leandro Gonçalves <leandro.rocha.goncalves@gmail.com>
2015-08-30 16:07:54 -0300

    atributos privados

--- models.py
+++ models.py
@@ -7,6 +7,27 @@ class Perfil(object):
         self.nome = nome
         self.telefone = telefone
         self.empresa = empresa
+        self.__curtidas = 0
 
     def imprimir(self):
-        print 'Nome: %s, telefone: %s, empresa: %s' % (self.nome, self.telefone, self.empresa)
+        print 'Nome: %s, telefone: %s, empresa: %s, curtidas: %s' % (self.nome, self.telefone, self.empresa, self.__curtidas)
+
+    def curtir(self):
+        self.__curtidas+=1
+
+    def obter_curtidas(self):
+        return self.__curtidas
+
+
+class Pessoa(object):
+    'Classe para pessoa'
+
+    def __init__(self, nome, peso, altura):
+        self.nome = nome
+        self.peso = peso
+        self.altura = altura
+
+    def imprimeImc(self):
+        imc = self.altura * self.altura
+        imc = self.peso / self.altura
+        print 'Imc de %s: %s' % (self.nome, imc)
