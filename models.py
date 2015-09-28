30924802fbc2240a97bddfaeb102f03954756d4e
Leandro Gonçalves <leandro.goncalves@hotelurbano.com.br>
2015-09-28 16:05:16 -0300

    trilha iniciando python

--- models.py
+++ models.py
@@ -18,6 +18,40 @@ class Perfil(object):
     def obter_curtidas(self):
         return self.__curtidas
 
+    @staticmethod
+    def gerar_perfis(nome_arquivo):
+        perfis = []
+
+        arquivo = open(nome_arquivo, 'r')
+
+        for linha in arquivo:
+            valores = linha.split(',')
+
+            if(len(valores) is not 3):
+                raise PerfilErro('Uma linha no arquivo deve ter 3 valores')
+
+            perfis.append(Perfil(*valores))
+
+        arquivo.close()
+
+        return perfis
+
+
+class Perfil_Vip(Perfil):
+    'classe para perfis de usuário vip'
+
+    def __init__(self, nome, telefone, empresa, apelido):
+        super(Perfil_Vip, self).__init__(nome, telefone, empresa)
+        self.apelido = apelido
+
+    def obter_creditos(self):
+        return super(Perfil_Vip, self).obter_curtidas() * 10.0
+
+class PerfilErro(Exception):
+    def __init(self, mensagem):
+        self.mensagem = mensagem
+    def __str__(self):
+        return repr(self.mensagem)
 
 class Pessoa(object):
     'Classe para pessoa'
