b96462cac718de0035e082846d9ccdcc89708ea1
Leandro Gonçalves <leandro.rocha.goncalves@gmail.com>
2015-10-24 16:32:04 -0200

    exemplo de template method

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
-
 class Calculador_de_impostos(object):
 
     def realiza_calculo(self, orcamento, imposto):
@@ -8,10 +7,23 @@ class Calculador_de_impostos(object):
 
 if __name__ == '__main__':
 
-    from orcamento import Orcamento
-    from impostos import ICMS, ISS
+    from orcamento import Orcamento, Item
 
-    orcamento = Orcamento(500.0)
+    # adicionado os impostos no import
+    from impostos import ISS, ICMS, ICPP, IKCV
+
+    orcamento = Orcamento()
+    # adicionando itens ao orçamento
+    orcamento.adiciona_item(Item('ITEM 1', 50))
+    orcamento.adiciona_item(Item('ITEM 2', 200))
+    orcamento.adiciona_item(Item('ITEM 3', 250))
+
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
+    print 'ISS e ICMS'
     calculador_de_impostos.realiza_calculo(orcamento, ISS())
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
+
+    # cálculo dos novos impostos
+    print 'ICPP e IKCV'
+    calculador_de_impostos.realiza_calculo(orcamento, ICPP()) # imprime 25.0
+    calculador_de_impostos.realiza_calculo(orcamento, IKCV()) # imprime 30.0
--- impostos.py
+++ impostos.py
@@ -1,4 +1,26 @@
 # -*- coding: UTF-8 -*-
+from abc import ABCMeta, abstractmethod
+
+class Imposto(object):
+    """docstring for Imposto"""
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def calcula(self, orcamento):
+        pass
+
+    @abstractmethod
+    def verifica_taxa_maxima(self, orcamento):
+        pass;
+
+    @abstractmethod
+    def aplica_taxa_maxima(self, orcamento):
+        pass;
+
+    @abstractmethod
+    def aplica_taxa_minima(self, orcamento):
+        pass;
+
 class ICMS(object):
     def calcula(self, orcamento):
         return orcamento.valor * 0.1
@@ -6,3 +28,41 @@ class ICMS(object):
 class ISS(object):
     def calcula(self, orcamento):
         return orcamento.valor * 0.06
+
+class ICPP(Imposto):
+    def calcula(self, orcamento):
+        if self.verifica_taxa_maxima(orcamento):
+            return self.aplica_taxa_maxima(orcamento)
+        else:
+            return self.aplica_taxa_minima(orcamento)
+
+    def verifica_taxa_maxima(self, orcamento):
+        return (orcamento.valor > 500)
+
+    def aplica_taxa_maxima(self, orcamento):
+        return orcamento.valor * 0.07
+
+    def aplica_taxa_minima(self, orcamento):
+        return orcamento.valor * 0.05
+
+class IKCV(Imposto):
+    def calcula(self, orcamento):
+        if self.verifica_taxa_maxima(orcamento):
+            return self.aplica_taxa_maxima(orcamento)
+        else:
+            return self.aplica_taxa_minima(orcamento)
+
+    def verifica_taxa_maxima(self, orcamento):
+        return (orcamento.valor > 500 and self.__tem_item_maior_que_100_reais(orcamento))
+
+    def aplica_taxa_maxima(self, orcamento):
+        return orcamento.valor * 0.10
+
+    def aplica_taxa_minima(self, orcamento):
+        return orcamento.valor * 0.06
+
+    def __tem_item_maior_que_100_reais(self, orcamento):
+        for item in orcamento.obtem_itens():
+            if (item.valor > 100):
+                return true
+            return false
